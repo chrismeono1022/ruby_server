06d5584fca952f46c9c60957dd02da6498deaa99
Chris Meono <chrismeono@Chriss-MacBook-Air-3.local>
2013-12-14 15:54:14 -0800

    parse path to map to content type

--- server.rb
+++ server.rb
@@ -1,10 +1,31 @@
-require 'socket' 
+require 'socket'
+require 'uri' 
+
+# all files server from only this dir
+ROOT = './public'
+
+CONTENT_TYPE = {
+  'html' => 'text/html',
+  'txt' => 'text/plain',
+  'png' => 'image/png',
+  'jpg' => 'image/jpeg'
+}
+
+# parse path and find content type; 'application/octet-stream' default for files without an extension (treated as binary) 
+def content_type(path)
+  extension = path.split(".").last
+  type = CONTENT_TYPE[extension] ||= 'application/octet-stream'
+end
+
+def find_file(request)
+  
+end
 
 # listen on localhost on port 2345
 server = TCPServer.new('localhost', 2345)
 
 # server ready to accept connections
-STDERR.puts "ready"
+STDERR.puts 'Server up and running'
 
 # continous loop to process connections
 loop do 
