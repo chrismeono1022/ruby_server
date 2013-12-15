3f634a3ec5ef77f5cb6662e5b15fea0b89a81bf5
Chris Meono <chrismeono@Chriss-MacBook-Air-3.local>
2013-12-14 16:47:39 -0800

    work in progress

---
+++ README.md
@@ -0,0 +1,10 @@
+Ruby Server
+
+Ruby server is a tiny web server written in pure ruby. I wanted to understand how webservers work under the hood, hence this project. 
+
+Future to do:
+
+* Add root homepage support
+* Implement HEAD response (per HTTP1.1 specs)
+* Error handling returning 500 status code
+* Add support for POST requests
--- server.rb
+++ server.rb
@@ -1,5 +1,4 @@
 require 'socket'
-require 'uri' 
 
 # all files server from only this dir
 ROOT = './public'
@@ -17,8 +16,10 @@ def content_type(path)
   type = CONTENT_TYPE[extension] ||= 'application/octet-stream'
 end
 
-def find_file(request)
-  
+# takes request - 'GET /anything HTTP/1.1' and parses it for file requested, then creates file path by adding './public'
+def requested_file(request)
+  file = request.split(" ")[1]
+  File.join(ROOT, file)
 end
 
 # listen on localhost on port 2345
@@ -28,7 +29,8 @@ server = TCPServer.new('localhost', 2345)
 STDERR.puts 'Server up and running'
 
 # continous loop to process connections
-loop do 
+loop do
+
   socket = server.accept
 
   # read request info
@@ -37,20 +39,36 @@ loop do
   # print to console
   STDERR.puts request
 
-  # test response
-  response = "Hello World!\n"
+  path = requested_file(request)
 
-  # a response is composed of a protocol/status code, content-type, content-length informing the client the size and type of data expected. HTTP is whitespace sensitive, and expects a new line "\r\n" at the end of each header
-  socket.print "HTTP/1.1 200 OK\r\n" + 
+  # check if file exists and isn't a directory before opening it
+  if File.exists?(path) && !File.directory?(path)
+    File.open(path, 'rb') do |file|
+      # a response is composed of a protocol/status code, content-type, content-length informing the client the size and type of data expected. HTTP is whitespace sensitive, and expects a new line "\r\n" at the end of each header
+      socket.print "HTTP/1.1 200 OK\r\n" + 
                 "Content-Type: text/pain\r\n" +
-                "content-Length: #{response.bytesize}\r\n"
+                "content-Length: #{file.size}\r\n"
                 "Connection: close\r\n"
                 
-  socket.print "\r\n" 
-  # add new line to separate header from response body as required by HTTP
+      # add new line to separate header from response body as required by HTTP
+      socket.print "\r\n"
 
-  # print response
-  socket.print response
+      # write file contents to socket
+      IO.copy_stream(file, socket)
+    end
+  else
+    message = "File not found\n"
+
+    # response with 404
+    socket.print "HTTP/1.1 404 Not Found\r\n" + 
+                "Content-Type: text/pain\r\n" +
+                "content-Length: #{message.bytesize}\r\n"
+                "Connection: close\r\n"
+
+    socket.print "\r\n"
+
+    socket.print message
+  end
 
   # terminate connection
   socket.close
