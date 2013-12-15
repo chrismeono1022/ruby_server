37b311c4544a7d6ba11c9398f946229443c1c56c
Chris Meono <chrismeono@Chriss-MacBook-Air-3.local>
2013-12-15 13:25:25 -0800

    implemented rack path clean up so user is unable to open files in other directories ie not in ./public

--- server.rb
+++ server.rb
@@ -18,15 +18,25 @@ end
 
 # takes request - 'GET /anything HTTP/1.1' and parses it for file requested, then creates file path by adding './public'
 def requested_file(request)
-  file = request.split(" ")[1]
-  File.join(ROOT, file)
+  uri = request.split(" ")[1]
+  clean_path = []
+  parts = uri.split("/")
+
+  # skip if path component includes current directory (".")
+  parts.each do |part|
+    next if part.empty? || part == '.'
+
+    # removes last component if path goes one directory level up (".."), pass; else append to clean_path
+    part == '..' ? next : clean_path << part
+  end
+  File.join(ROOT, *clean_path)
 end
 
 # listen on localhost on port 2345
 server = TCPServer.new('localhost', 2345)
 
 # server ready to accept connections
-STDERR.puts 'Server up and running'
+STDERR.puts 'Server running on localhost:2345'
 
 # continous loop to process connections
 loop do
@@ -46,7 +56,7 @@ loop do
     File.open(path, 'rb') do |file|
       # a response is composed of a protocol/status code, content-type, content-length informing the client the size and type of data expected. HTTP is whitespace sensitive, and expects a new line "\r\n" at the end of each header
       socket.print "HTTP/1.1 200 OK\r\n" + 
-                "Content-Type: text/pain\r\n" +
+                "Content-Type: #{content_type(path)}\r\n" +
                 "content-Length: #{file.size}\r\n"
                 "Connection: close\r\n"
                 
